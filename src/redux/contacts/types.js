72fdf543d64ed022b82f444504d1186551acf419
Natalia Sabadysh <nataliasabadysh@gmail.com>
2021-10-24 23:17:27 +0300

    hw

--- src/redux/contacts/types.js
+++
@@ -1,7 +0,0 @@
-export const FILL_CONTACTS = 'contact/FILL_CONTACTS';
-
-export const STOP_LOADING = "contact/STOP_LOADING";
-export const START_LOADING = "contact/START_LOADING";
-
-export const SET_ERROR = "contact/SET_ERROR";
-
